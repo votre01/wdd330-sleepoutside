06e28d89fe43d161c09f91357e1ac42ef8c1bcf3
Trevor <ngw21001@byui.edu>
2023-11-13 15:00:47 +0200

    individual2

---
+++ src/js/ProductListing.mjs
@@ -0,0 +1,29 @@
+function productCardTemplate(product) {
+    return `<li class="product-card">
+      <a href="product_pages/index.html?product=">
+        <img src="" alt="Image of ">
+        <h3 class="card__brand"></h3>
+        <h2 class="card__name"></h2>
+        <p class="product-card__price">$</p>
+      </a>
+    </li>`
+  }
+
+export default class ProductListing {
+    constructor(category, dataSource, listElement) {
+      this.category = category;
+      this.dataSource = dataSource;
+      this.listElement = listElement;
+    }
+
+    async init() {
+      const list = await this.dataSource.getData();
+    }
+
+    renderList(list) {
+        const htmlStrings = list.map(productCardTemplate);
+        this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
+    }
+  }
+
+  
